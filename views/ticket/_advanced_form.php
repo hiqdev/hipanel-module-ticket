263e914a98da0751b6bd83baea2454d912e97d22
Andrey Klochok <andreyklochok@gmail.com>
2015-05-13 19:14:31 +0300

    Change select2 to combo2

--- views/ticket/_advanced_form.php
+++ views/ticket/_advanced_form.php
@@ -5,9 +5,9 @@
  * @copyright Copyright (c) 2015 HiQDev
  */
 use hipanel\widgets\Combo2;
+use hipanel\widgets\DefaultCombo2;
 use yii\helpers\Url;
 use yii\web\JsExpression;
-use kartik\widgets\Select2;
 
 ?>
     <!-- Topics -->
@@ -15,12 +15,14 @@ use kartik\widgets\Select2;
     $model->topics = 'general';
 else
     $model->topics = array_keys($model->topics);
-print $form->field($model, 'topics')->widget(Select2::classname(), [
-    'data' => array_merge(["" => ""], $topic_data),
-    'options' => ['placeholder' => 'Select a topics ...', 'multiple' => true],
-    'pluginOptions' => [
-        'allowClear' => true,
-    ],
+print $form->field($model, 'topics')->widget(Combo2::classname(), [
+    'type' => DefaultCombo2::className(),
+    'fieldOptions' => [
+        'pluginOptions' => [
+            'allowClear' => true,
+            'data' => array_merge(["" => ""], $topic_data),
+        ],
+    ]
 ]); ?>
 <div class="row">
     <div class="col-md-6">
@@ -28,12 +30,13 @@ print $form->field($model, 'topics')->widget(Select2::classname(), [
         <?php
         if ($model->isNewRecord)
             $model->state = 'opened';
-        print $form->field($model, 'state')->widget(Select2::classname(), [
-            'data' => array_merge(["" => ""], $state_data),
-            'options' => ['placeholder' => 'Select a state ...'],
-            'pluginOptions' => [
-                'allowClear' => true,
-            ],
+        print $form->field($model, 'state')->widget(Combo2::classname(), [
+            'type' => DefaultCombo2::className(),
+            'fieldOptions' => [
+                'pluginOptions' => [
+                    'data' => array_merge(["" => ""], $state_data),
+                ]
+            ]
         ]); ?>
     </div>
     <div class="col-md-6">
@@ -41,12 +44,14 @@ print $form->field($model, 'topics')->widget(Select2::classname(), [
         <?php
         if ($model->isNewRecord)
             $model->priority = 'medium';
-        print $form->field($model, 'priority')->widget(Select2::classname(), [
-            'data' => array_merge(["" => ""], $priority_data),
-            'options' => ['placeholder' => 'Select a priority ...'],
-            'pluginOptions' => [
-                'allowClear' => true,
-            ],
+        print $form->field($model, 'priority')->widget(Combo2::classname(), [
+            'type' => DefaultCombo2::className(),
+            'fieldOptions' => [
+                'pluginOptions' => [
+                    'data' => array_merge(["" => ""], $priority_data),
+                    'allowClear' => true,
+                ]
+            ]
         ]); ?>
     </div>
 </div>
@@ -54,79 +59,18 @@ print $form->field($model, 'topics')->widget(Select2::classname(), [
     <!-- Responsible -->
 <?= $form->field($model, 'responsible_id')->widget(Combo2::classname(), [
     'type' => \hipanel\modules\client\assets\combo2\Manager::className()
-//    'options' => ['placeholder' => 'Search for a responsible ...'],
-//    'pluginOptions' => [
-//        'allowClear' => true,
-//        'minimumInputLength' => 3,
-//        'ajax' => [
-//            'url' => Url::to(['/client/client/can-manage-list']),
-//            'dataType' => 'json',
-//            'data' => new JsExpression('function(term,page) { return {search:term}; }'),
-//            'results' => new JsExpression('function(data,page) { return {results:data.results}; }'),
-//        ],
-//        'initSelection' => new JsExpression('function (elem, callback) {
-//                                                            var id=$(elem).val();
-//                                                            $.ajax("' . Url::to(['/client/client/can-manage-list']) . '?id=" + id, {
-//                                                                dataType: "json"
-//                                                            }).done(function(data) {
-//                                                                callback(data.results);
-//                                                            });
-//                                                        }')
-//    ],
 ]); ?>
 
 <?php if ($model->scenario == 'insert') : ?>
     <?= $form->field($model, 'watchers')->widget(Combo2::classname(), [
-        'type' => 'client'
-//        'options' => ['placeholder' => 'Select watchers ...', 'multiple' => true],
-//        'pluginOptions' => [
-//            'allowClear' => true,
-//            'minimumInputLength' => 3,
-//            'multiple' => true,
-//            'ajax' => [
-//                'url' => Url::to(['/client/client/can-manage-list']),
-//                'dataType' => 'json',
-//                'data' => new JsExpression('function(term,page) { return {search:term}; }'),
-//                'results' => new JsExpression('function(data,page) { return {results:data.results}; }'),
-//            ],
-//            'initSelection' => new JsExpression('function (elem, callback) {
-//                                                            var id=$(elem).val();
-//                                                            $.ajax("' . Url::to(['/client/client/can-manage-list']) . '?id=" + id, {
-//                                                                dataType: "json"
-//                                                            }).done(function(data) {
-//                                                                callback(data.results);
-//                                                            });
-//                                                        }')
-//        ],
+        'type' => \hipanel\modules\client\assets\combo2\Manager::className()
     ]); ?>
 <?php endif; ?>
 
 <?php
 if ($model->isNewRecord)
     $model->recipient_id = \Yii::$app->user->identity->id;
-
-print $form->field($model, 'recipient_id')->widget(Select2::classname(), [
-    'options' => ['placeholder' => 'Search for a responsible ...'],
-    'pluginOptions' => [
-        'allowClear' => true,
-        'minimumInputLength' => 3,
-        'ajax' => [
-            'url' => Url::to(['/client/client/client-all-list']),
-            'dataType' => 'json',
-            'data' => new JsExpression('function(term,page) { return {search:term}; }'),
-            'results' => new JsExpression('function(data,page) { return {results:data.results}; }'),
-        ],
-        'initSelection' => new JsExpression('function (elem, callback) {
-                                                            var id=$(elem).val();
-                                                            $.ajax("' . Url::to(['/client/client/client-all-list']) . '?id=" + id, {
-                                                                dataType: "json"
-                                                            }).done(function(data) {
-                                                                callback(data.results);
-                                                            });
-                                                        }')
-    ],
-]); ?>
-
+print $form->field($model, 'recipient_id')->widget(Combo2::classname(), ['type' => \hipanel\modules\client\assets\combo2\Client::className()]); ?>
 
 <?php if ($model->scenario != 'answer') : ?>
     <?= $form->field($model, 'spent')->widget(kartik\widgets\TimePicker::className(), [
