28b054332d2c1eea6de6af569042de647de63dc7
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-04-22 21:39:25 +0300

    Added ConditionalFormWidget

---
+++ src/widgets/ConditionalFormWidget.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace hipanel\modules\ticket\widgets;
+
+use Yii;
+use yii\base\InvalidCallException;
+use yii\base\Widget;
+use yii\widgets\ActiveForm;
+
+/**
+ * Class ConditionalFormWidget is designed for the following use case in ticket module:
+ * On ticket create page all fields must be wrapped in single form,
+ * however on the view page each block must be wrapped in a separate form in order to prevent
+ * logical conflicts. With this widget we can use the same view files for both pages.
+ *
+ * Use example:
+ *
+ * ```php
+ * // Use `$form` without any changes, or creates new one with `options`
+ * $form = ConditionalFormWidget::begin([
+ *     'form' => isset($form) ? $form : null,
+ *     'options' => [
+ *         'action'  => $action,
+ *         'options' => ['enctype' => 'multipart/form-data'],
+ *     ]
+ * ]);
+ *
+ * $form->field($model, 'input');
+ *
+ * ConditionalFormWidget::end(); // Do not use $form->end() here!
+ * ```
+ *
+ * IMPORTANT: the `::begin()` method returns instance of [[ActiveForm]] instead of this widget object.
+ * Is is by design and done in order to give transparent variables.
+ *
+ * This class must be used only with `::begin()` and `::end()` methods.
+ *
+ * @author Dmitry Naumenko <d.naumenko.a@gmail.com>
+ * @package hipanel\modules\ticket\widgets
+ */
+class ConditionalFormWidget extends Widget
+{
+    /**
+     * @var ActiveForm
+     */
+    public $form;
+
+    /**
+     * @var array options that will be used to create [[$form]] object
+     */
+    public $options;
+
+    /**
+     * @var boolean
+     */
+    protected $closeForm = false;
+
+    /**
+     * @param array $config
+     * @return ActiveForm
+     */
+    public static function begin($config = [])
+    {
+        /** @var $widget static */
+        $widget = parent::begin($config);
+        return $widget->form;
+    }
+
+    /** @inheritdoc */
+    public function init()
+    {
+        if (!isset($this->form)) {
+            $this->closeForm = true;
+            $this->form = ActiveForm::begin($this->options);
+        }
+    }
+
+    /** @inheritdoc */
+    public function run()
+    {
+        if ($this->closeForm) {
+            $this->form->end();
+        }
+    }
+
+    /**
+     * @throws InvalidCallException
+     * @internal
+     */
+    public static function widget($config = [])
+    {
+        throw new InvalidCallException('Class must be used with `::begin()` and `::end()` methods.');
+    }
+}
