efae406fe6aff3b1e1669e4dfc7d79f75be591a6
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-04-22 21:24:00 +0300

    Added AnswerController

---
+++ src/controllers/AnswerController.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * HiPanel tickets module
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-ticket
+ * @package   hipanel-module-ticket
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\ticket\controllers;
+
+
+use hipanel\actions\RedirectAction;
+use hipanel\actions\SmartPerformAction;
+use Yii;
+
+class AnswerController extends \hipanel\base\CrudController
+{
+    public function actions()
+    {
+        return [
+            'update'   => [
+                'class'      => SmartPerformAction::class,
+                'scenario'   => 'update',
+                'success'    => Yii::t('hipanel/ticket', 'Comment was updated'),
+                'POST'  => [
+                    'save'    => true,
+                    'success' => [
+                        'class'       => RedirectAction::class,
+                        'url'        => function () {
+                            $answer = Yii::$app->request->post('Answer');
+                            return ['@ticket/view', 'id' => $answer['id']];
+                        },
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function prepareRefs()
+    {
+        return [
+            'topic_data'    => $this->getRefs('topic,ticket'),
+            'state_data'    => $this->GetClassRefs('state'),
+            'priority_data' => $this->getPriorities(),
+        ];
+    }
+}
