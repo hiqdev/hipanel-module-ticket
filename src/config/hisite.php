ac577a5fefb22984a6ac07fe1481932ae0fc4fd1
Andrii Vasyliev <sol@hiqdev.com>
2016-05-18 14:08:50 +0000

    fixed composer.json

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -1,20 +1,29 @@
 <?php
 
+/*
+ * HiPanel tickets module
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-ticket
+ * @package   hipanel-module-ticket
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
 return [
     'aliases' => [
-        '@ticket' => '/ticket/ticket',
+        '@ticket'        => '/ticket/ticket',
         '@ticket/answer' => '/ticket/answer',
     ],
     'modules' => [
         'ticket' => [
-            'class' => 'hipanel\modules\ticket\Module',
+            'class' => \hipanel\modules\ticket\Module::class,
         ],
     ],
     'components' => [
         'i18n' => [
             'translations' => [
                 'hipanel/ticket*' => [
-                    'class' => 'yii\i18n\PhpMessageSource',
+                    'class' => \yii\i18n\PhpMessageSource::class,
                     'basePath' => '@hipanel/modules/ticket/messages',
                     'fileMap' => [
                         'hipanel/ticket' => 'ticket.php',
@@ -24,7 +33,7 @@ return [
         ],
         'menuManager' => [
             'menus' => [
-                'ticket' => 'hipanel\modules\ticket\SidebarMenu',
+                'ticket' => \hipanel\modules\ticket\SidebarMenu::class,
             ],
         ],
     ],
