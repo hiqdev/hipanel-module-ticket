f7f941e3d19565240acc49a8725919bcdbd8a80f
tofid <andreyklochok@gmail.com>
2016-12-06 19:04:27 +0200

    Hid serach fields from client

--- src/views/ticket/_search.php
+++ src/views/ticket/_search.php
@@ -14,18 +14,21 @@ use yii\helpers\Html;
     <?= $search->field('anytext_like') ?>
 </div>
 
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('author_id')->widget(ClientCombo::class); ?>
-</div>
+<?php if (Yii::$app->user->can('support')) : ?>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('author_id')->widget(ClientCombo::class); ?>
+    </div>
+
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('recipient_id')->widget(ClientCombo::class, [
+            'clientType' => 'client',
+        ]) ?>
+    </div>
+<?php endif; ?>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('recipient_id')->widget(ClientCombo::class, [
-        'clientType' => 'client',
-    ]) ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
     <div class="form-group">
-        <?= Html::label(Yii::t('hipanel', 'Date'))?>
+        <?= Html::label(Yii::t('hipanel', 'Date')) ?>
         <?= DatePicker::widget([
             'model' => $search->model,
             'attribute' => 'time_from',
@@ -49,16 +52,37 @@ use yii\helpers\Html;
         ],
     ]) ?>
 </div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('responsible_id')->widget(ClientCombo::class, [
-        'clientType' => 'manager',
-        'pluginOptions' => [
-            'select2Options' => [
-                'multiple' => true,
+
+<?php if (Yii::$app->user->can('support')) : ?>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('responsible_id')->widget(ClientCombo::class, [
+            'clientType' => 'manager',
+            'pluginOptions' => [
+                'select2Options' => [
+                    'multiple' => true,
+                ],
             ],
-        ],
-    ]); ?>
-</div>
+        ]); ?>
+    </div>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('priority')->widget(StaticCombo::class, [
+            'data' => $priority_data,
+            'hasId' => true,
+        ]) ?>
+    </div>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?php echo $search->field('watchers')->widget(ClientCombo::class, [
+            'clientType' => ['manager', 'admin', 'owner'],
+            'hasId' => true,
+            'pluginOptions' => [
+                'select2Options' => [
+                    'multiple' => true,
+                ],
+            ],
+        ]) ?>
+    </div>
+<?php endif; ?>
+
 <div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('topics')->widget(StaticCombo::class, [
         'data' => $topic_data,
@@ -69,21 +93,4 @@ use yii\helpers\Html;
             ],
         ],
     ]); ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('priority')->widget(StaticCombo::class, [
-        'data' => $priority_data,
-        'hasId' => true,
-    ]) ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?php echo $search->field('watchers')->widget(ClientCombo::class, [
-        'clientType' => ['manager', 'admin', 'owner'],
-        'hasId' => true,
-        'pluginOptions' => [
-            'select2Options' => [
-                'multiple' => true,
-            ],
-        ],
-    ]) ?>
 </div>
