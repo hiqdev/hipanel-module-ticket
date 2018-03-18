2a8716be86c99e97f0441caf1c56cf909681d866
Yurii Myronchuk <bladeroot@gmail.com>
2018-03-18 15:25:16 +0000

    fix columns

--- src/grid/StatisticGridView.php
+++ src/grid/StatisticGridView.php
@@ -13,8 +13,11 @@ namespace hipanel\modules\ticket\grid;
 use hipanel\grid\MainColumn;
 use hipanel\grid\SwitchColumn;
 use hipanel\modules\client\grid\ClientColumn;
+use hipanel\widgets\ClientSellerLink;
 use hipanel\modules\ticket\menus\TemplateActionsMenu;
 use hiqdev\yii2\menus\grid\MenuColumn;
+use yii\helpers\Html;
+use yii\helpers\Url;
 use Yii;
 
 class StatisticGridView extends \hipanel\grid\BoxedGridView
@@ -24,15 +27,29 @@ class StatisticGridView extends \hipanel\grid\BoxedGridView
         return array_merge(parent::columns(), [
             'client_id' => [
                 'class' => ClientColumn::class,
+                'label' => Yii::t('hipanel', 'Client'),
+                'idAttribute' => 'client_id',
+                'sortAttribute' => 'client',
+                'attribute' => 'client_id',
+                'value' => function ($model) {
+                    return ClientSellerLink::widget(compact('model'));
+                },
             ],
             'spent' => [
+                'label' => Yii::t('hipanel:ticket', 'Spent'),
+                'filter' => false,
                 'value' => function($model) {
-                    return $model->spent;
+                    return Yii::$app->formatter->asDuration($model->spent * 60);
                 },
             ],
             'tickets' => [
+                'format' => 'html',
                 'value' => function($model) {
-                    return '';
+                    foreach (explode(",", $model->thread_ids) as $thread) {
+                        $threads[$thread] = Html::a($thread, Url::to(['@ticket/view', 'id' => $thread]));
+                    }
+                    ksort($threads);
+                    return implode(" ", $threads);
                 }
             ],
         ]);
