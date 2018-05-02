fbd34c79f9303d97737f3bddef10a54860cf81c3
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-05-02 20:09:23 +0300

    Trying to implemet tests

---
+++ tests/_support/AcceptanceTester.php
@@ -0,0 +1,24 @@
+<?php
+namespace hipanel\modules\ticket\tests\_support;
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class AcceptanceTester extends \hipanel\tests\_support\AcceptanceTester
+{
+   /**
+    * Define custom actions here
+    */
+}
