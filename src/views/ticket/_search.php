41835381392375e6f5f7fe3552f1a2ba3e47275e
Yurii Myronchuk <bladeroot@gmail.com>
2018-05-31 14:37:27 +0000

    wix search with watchers

--- src/views/ticket/_search.php
+++ src/views/ticket/_search.php
@@ -67,7 +67,7 @@ use yii\helpers\Html;
         ]) ?>
     </div>
     <div class="col-md-4 col-sm-6 col-xs-12">
-        <?php echo $search->field('watchers')->widget(ClientCombo::class, [
+        <?php echo $search->field('watcher_ids')->widget(ClientCombo::class, [
             'clientType' => ['manager', 'admin', 'owner'],
             'hasId' => true,
             'pluginOptions' => [
