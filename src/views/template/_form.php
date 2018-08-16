f9fa245edf6e103b36da5da1298e4ac5f0731e91
Yurii Myronchuk <bladeroot@gmail.com>
2018-08-16 12:05:58 +0300

    Added subject to ticket templates update/create (#14)

--- src/views/template/_form.php
+++ src/views/template/_form.php
@@ -30,43 +30,44 @@ $form = ActiveForm::begin([
                         <div class="box-body">
                             <div class="form-instance">
                                 <?php if ($model->isNewRecord) : ?>
-                                    <?php $model->id = $i; ?>
+                                    <?php $model->id = $i ?>
                                 <?php endif ?>
-                                <?= Html::activeHiddenInput($model, "[$i]id"); ?>
-                                <?= $form->field($model, "[$i]name"); ?>
-                                <?= $form->field($model, "[$i]is_published")->checkbox(); ?>
+                                <?= $form->field($model, "[$i]id")->hiddenInput()->label(false) ?>
+                                <?= $form->field($model, "[$i]name") ?>
+                                <?= $form->field($model, "[$i]is_published")->checkbox() ?>
                             </div>
                         </div>
                     </div>
                 </div>
-                <?php $data = $model->getAddedTexts(); ?>
-                <?php $j = 0; ?>
+                <?php $data = $model->getAddedTexts() ?>
+                <?php $j = 0 ?>
                 <?php foreach ($data as $translation) : ?>
                     <div class="col-md-12">
                         <?php $box = Box::begin(['renderBody' => false]); ?>
-                            <?php $box->beginHeader(); ?>
+                            <?php $box->beginHeader() ?>
                                 <?= $box->renderTitle(Yii::t('hipanel:ticket', 'Translation: {language}', [
                                     'language' => Yii::t('hipanel', $translation->lang),
-                                ])); ?>
-                            <?php $box->endHeader(); ?>
-                            <?php $box->beginBody(); ?>
+                                ])) ?>
+                            <?php $box->endHeader() ?>
+                            <?php $box->beginBody() ?>
                                 <?php if ($translation->isNewRecord) : ?>
-                                    <?php $translation->article_id = $i; ?>
+                                    <?php $translation->article_id = $i ?>
                                 <?php endif ?>
-                                <?= Html::activeHiddenInput($translation, "[$i][$j]lang"); ?>
-                                <?= Html::activeHiddenInput($translation, "[$i][$j]article_id"); ?>
-                                <?= $form->field($translation, "[$i][$j]text")->textarea(['rows' => 6])->label(false); ?>
-                            <?php $box->endBody(); ?>
-                        <?php $box->end(); ?>
+                                <?= $form->field($translation, "[$i][$j]lang")->hiddenInput()->label(false) ?>
+                                <?= $form->field($translation, "[$i][$j]article_id")->hiddenInput()->label(false) ?>
+                                <?= $form->field($translation, "[$i][$j]title")->label(Yii::t('hipanel:ticket', 'Subject')) ?>
+                                <?= $form->field($translation, "[$i][$j]text")->textarea(['rows' => 6])->label(false) ?>
+                            <?php $box->endBody() ?>
+                        <?php $box->end() ?>
                     </div>
-                <?php ++$j; ?>
-                <?php endforeach; ?>
+                <?php ++$j ?>
+                <?php endforeach ?>
 
             </div>
-        <?php endforeach; ?>
+        <?php endforeach ?>
     </div>
-<?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']) ?>
+    <?= Html::submitButton(Yii::t('hipanel', 'Save'), ['class' => 'btn btn-success']) ?>
     &nbsp;
-<?= Html::button(Yii::t('hipanel', 'Cancel'), ['class' => 'btn btn-default', 'onclick' => 'history.go(-1)']) ?>
+    <?= Html::button(Yii::t('hipanel', 'Cancel'), ['class' => 'btn btn-default', 'onclick' => 'history.go(-1)']) ?>
 
-<?php ActiveForm::end();
+<?php $form::end() ?>
