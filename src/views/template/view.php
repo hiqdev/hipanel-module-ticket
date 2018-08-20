561176e68dd09b16722dc00091efb448f9883e42
Yurii Myronchuk <bladeroot@gmail.com>
2018-08-20 16:42:09 +0300

    csfixed

--- src/views/template/view.php
+++ src/views/template/view.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'bodyOptions' => [
                 'class' => 'no-padding',
             ],
-        ]); ?>
+        ]) ?>
         <div class="profile-user-img text-center">
             <i class="fa fa-file-text-o fa-5x"></i>
         </div>
@@ -40,12 +40,12 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="profile-usermenu">
             <?= TemplateDetailMenu::widget(['model' => $model]) ?>
         </div>
-        <?php Box::end(); ?>
-            <?php $box = Box::begin(['renderBody' => false]); ?>
-            <?php $box->beginHeader(); ?>
-                <?= $box->renderTitle(Yii::t('hipanel:ticket', 'Template details')); ?>
-            <?php $box->endHeader(); ?>
-            <?php $box->beginBody(); ?>
+        <?php Box::end() ?>
+            <?php $box = Box::begin(['renderBody' => false]) ?>
+            <?php $box->beginHeader() ?>
+                <?= $box->renderTitle(Yii::t('hipanel:ticket', 'Template details')) ?>
+            <?php $box->endHeader() ?>
+            <?php $box->beginBody() ?>
                 <?= TemplateGridView::detailView([
                     'model' => $model,
                     'boxed' => false,
@@ -54,32 +54,31 @@ $this->params['breadcrumbs'][] = $this->title;
                         'name',
                         'is_published',
                     ],
-                ]); ?>
-            <?php $box->endBody(); ?>
-        <?php $box->end(); ?>
-        ?>
+                ]) ?>
+            <?php $box->endBody() ?>
+        <?php $box->end() ?>
     </div>
 
     <div class="col-md-9">
         <div class="row">
             <?php foreach ($model->texts as $translation) : ?>
                 <div class="col-md-12">
-                    <?php $box = Box::begin(['renderBody' => false]); ?>
-                        <?php $box->beginHeader(); ?>
+                    <?php $box = Box::begin(['renderBody' => false]) ?>
+                        <?php $box->beginHeader() ?>
                             <?= $box->renderTitle(Yii::t('hipanel:ticket', 'Translation: {language}', [
                                 'language' => Yii::t('hipanel', $translation->lang),
-                            ])); ?>
-                            <?php $box->endHeader(); ?>
-                            <?php $box->beginBody(); ?>
+                            ])) ?>
+                            <?php $box->endHeader() ?>
+                            <?php $box->beginBody() ?>
                                 <?php if (!empty($translation->text)) : ?>
-                                    <?= Markdown::process($translation->text); ?>
+                                    <?= Markdown::process($translation->text) ?>
                                 <?php else: ?>
                                     <?= Html::tag('span', Yii::t('hipanel:ticket', 'No translation'), [
                                         'class' => 'text-danger',
-                                    ]); ?>
+                                    ]) ?>
                                 <?php endif ?>
-                        <?php $box->endBody(); ?>
-                    <?php $box->end(); ?>
+                        <?php $box->endBody() ?>
+                    <?php $box->end() ?>
                 </div>
             <?php endforeach ?>
         </div>
