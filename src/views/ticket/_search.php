e65b5036de15e4bc249283a7c8b6c8d784e5ab9f
Andrey Klochok <andreyklochok@gmail.com>
2019-10-30 17:16:26 +0200

    Added ture search by thread`s messages, because Margo says the RCP has it and she wants the same functionality in the HiPanel

--- src/views/ticket/_search.php
+++ src/views/ticket/_search.php
@@ -16,7 +16,7 @@ use yii\helpers\Html;
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('message_like') ?>
+    <?= $search->field('message') ?>
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
