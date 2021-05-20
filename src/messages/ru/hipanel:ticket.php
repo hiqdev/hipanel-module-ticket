1173a8237aa2ccd3fd69fb2d859672b860c401b7
Andrii Vasyliev <sol@hiqdev.com>
2021-05-20 12:02:06 +0300

    minor

--- src/messages/ru/hipanel:ticket.php
+++ src/messages/ru/hipanel:ticket.php
@@ -146,5 +146,5 @@ return [
 
     'Are you sure you want to close these tickets?' => 'Вы действительно хотите закрыть эти тикеты?',
     'Ticket numbers separated by commas' => 'Номера тикитов, разделенные запятыми',
-    'Hide payment ticket' => 'Скрыть напоминание о оплате',
+    'Hide payment tickets' => 'Скрыть напоминания о оплате',
 ];
