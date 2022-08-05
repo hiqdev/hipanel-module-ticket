8af3ce7efdd370566e7f4c5d3d0ac0017c892df8
Yurii Myronchuk <bladeroot@hiqdev.com>
2022-08-05 05:31:35 +0000

    fix

--- src/views/ticket/_clientInfo.php
+++ src/views/ticket/_clientInfo.php
@@ -25,46 +25,46 @@ $this->registerCss('
 }
 ');
 
+if (!is_null($client) {
+    if ($client->login !== 'anonym') {
+        if ($client instanceof ClientRelationFreeStub) {
+            $loader = \hipanel\widgets\AsyncLoader::widget([
+                'route' => ['@ticket/render-client-info', 'id' => $client->id],
+                'containerSelector' => '.b-ticket-client-info',
+            ]);
+        }
 
-if ($client->login !== 'anonym') {
-    if ($client instanceof ClientRelationFreeStub) {
-        $loader = \hipanel\widgets\AsyncLoader::widget([
-            'route' => ['@ticket/render-client-info', 'id' => $client->id],
-            'containerSelector' => '.b-ticket-client-info',
-        ]);
+        $linkToClient = Html::a(
+            '<i class="fa fa-info-circle" style="font-size: 120%"></i> &nbsp;&nbsp;' . Yii::t('hipanel:ticket', 'Client details'),
+            ['@client/view', 'id' => $client->id],
+            ['class' => 'btn bg-olive btn-sm btn-block btn-flat']
+        );
+    } else {
+        $loader = '';
+        $linkToClient = '';
     }
-
-    $linkToClient = Html::a(
-        '<i class="fa fa-info-circle" style="font-size: 120%"></i> &nbsp;&nbsp;' . Yii::t('hipanel:ticket', 'Client details'),
-        ['@client/view', 'id' => $client->id],
-        ['class' => 'btn bg-olive btn-sm btn-block btn-flat']
-    );
-} else {
-    $loader = '';
-    $linkToClient = '';
-}
 ?>
 
-<div class="col-md-12 b-ticket-client-info">
-    <?= MainDetails::widget([
-        'image' => $this->render('//layouts/gravatar', ['email' => $client->email, 'size' => 60, 'alt' => '']),
-        'title' => ClientSellerLink::widget(['model' => $client]),
-        'subTitle' => Yii::t('hipanel:client', ucfirst((string)$client->type)),
-        'menu' => (isset($loader) ? '<div class="overlay-wrapper">' . $loader . '<div class="table-responsive ">' : '') . ClientGridView::detailView([
-                'model' => $client,
-                'boxed' => false,
-                'columns' => array_filter(
-                    $client->login === 'anonym' ? ['name', 'email'] : [
-                        'name', 'email', 'messengers', 'country', 'language', 'state',
-                        Yii::$app->user->can('bill.read') ? 'balance' : null,
-                        Yii::$app->user->can('bill.read') ? 'credit' : null,
-                        class_exists(\hipanel\modules\server\Module::class) ? 'servers_spoiler' : null,
-                        class_exists(\hipanel\modules\domain\Module::class) ? 'domains_spoiler' : null,
-                        class_exists(\hipanel\modules\hosting\Module::class) ? 'hosting' : null,
-                        class_exists(\hipanel\modules\finance\Module::class) && Yii::$app->user->can('plan.read') ? 'targets_spoiler' : null,
-                    ]
-                ),
-            ]) . '</div>' . $linkToClient . '</div>',
-    ]) ?>
-</div>
-
+    <div class="col-md-12 b-ticket-client-info">
+        <?= MainDetails::widget([
+            'image' => $this->render('//layouts/gravatar', ['email' => $client->email, 'size' => 60, 'alt' => '']),
+            'title' => ClientSellerLink::widget(['model' => $client]),
+            'subTitle' => Yii::t('hipanel:client', ucfirst((string)$client->type)),
+            'menu' => (isset($loader) ? '<div class="overlay-wrapper">' . $loader . '<div class="table-responsive ">' : '') . ClientGridView::detailView([
+                    'model' => $client,
+                    'boxed' => false,
+                    'columns' => array_filter(
+                        $client->login === 'anonym' ? ['name', 'email'] : [
+                            'name', 'email', 'messengers', 'country', 'language', 'state',
+                            Yii::$app->user->can('bill.read') ? 'balance' : null,
+                            Yii::$app->user->can('bill.read') ? 'credit' : null,
+                            class_exists(\hipanel\modules\server\Module::class) ? 'servers_spoiler' : null,
+                            class_exists(\hipanel\modules\domain\Module::class) ? 'domains_spoiler' : null,
+                            class_exists(\hipanel\modules\hosting\Module::class) ? 'hosting' : null,
+                            class_exists(\hipanel\modules\finance\Module::class) && Yii::$app->user->can('plan.read') ? 'targets_spoiler' : null,
+                        ]
+                    ),
+                ]) . '</div>' . $linkToClient . '</div>',
+        ]) ?>
+    </div>
+<?php } ?>
